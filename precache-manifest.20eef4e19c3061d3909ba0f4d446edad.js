2a3c50f4392ff4bda3896680e70cc2b21de42d11
mabochen <bochenma@cs.washington.edu>
2020-07-09 16:16:05 -0700

    Updates

---
+++ precache-manifest.20eef4e19c3061d3909ba0f4d446edad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719851577cb947476dd67cabd8e9a379",
+    "url": "/steve_ma_uwcse.io/index.html"
+  },
+  {
+    "revision": "2d2c12792b831f924862",
+    "url": "/steve_ma_uwcse.io/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "57c5b54927fc7503bb3f",
+    "url": "/steve_ma_uwcse.io/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "358ddceed820f8f464be",
+    "url": "/steve_ma_uwcse.io/static/css/main.d0293cf6.chunk.css"
+  },
+  {
+    "revision": "888b4b5830db14922638",
+    "url": "/steve_ma_uwcse.io/static/js/2.9179c065.chunk.js"
+  },
+  {
+    "revision": "2d2c12792b831f924862",
+    "url": "/steve_ma_uwcse.io/static/js/3.32bfc5ad.chunk.js"
+  },
+  {
+    "revision": "57c5b54927fc7503bb3f",
+    "url": "/steve_ma_uwcse.io/static/js/4.05955055.chunk.js"
+  },
+  {
+    "revision": "358ddceed820f8f464be",
+    "url": "/steve_ma_uwcse.io/static/js/main.82db4167.chunk.js"
+  },
+  {
+    "revision": "92abf19c6fad8b73dc15",
+    "url": "/steve_ma_uwcse.io/static/js/runtime-main.c2adb75b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/steve_ma_uwcse.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/steve_ma_uwcse.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d725bfec3990c575c2e762c55ac2bb3f",
+    "url": "/steve_ma_uwcse.io/static/media/TikTok_logo.d725bfec.png"
+  },
+  {
+    "revision": "0e42d7fbf6dbf43be6adbea5fe082388",
+    "url": "/steve_ma_uwcse.io/static/media/circle-cropped.0e42d7fb.png"
+  },
+  {
+    "revision": "ad952228abdc6e35de22b2483881a9d9",
+    "url": "/steve_ma_uwcse.io/static/media/gemsec.ad952228.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/steve_ma_uwcse.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "8c90e2c6ea79cc02ba2f12764ec7380d",
+    "url": "/steve_ma_uwcse.io/static/media/uwcse.8c90e2c6.jpg"
+  },
+  {
+    "revision": "20840525ed6fc14d78fac3318c419d1c",
+    "url": "/steve_ma_uwcse.io/static/media/uwcse2.20840525.png"
+  }
+]);
